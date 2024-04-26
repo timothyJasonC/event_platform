f3c5b7482f7cecf55c22d60dfb14d826f1ef3396
Timothy Jason <timothycaviezel@gmail.com>
2024-04-26 22:14:15 +0700

    second commit

--- src/app/api/webhook/clerk/route.ts
+++ src/app/api/webhook/clerk/route.ts
@@ -1,7 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent, clerkClient } from '@clerk/nextjs/server'
-import { createUser, deleteUser, updateUser } from '@/lib/actions/user.action'
+import { createUser, deleteUser, updateUser } from '../../../../lib/actions/user.action'
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
