255e955f8f59aed9a78763cea664188207c308d8
Timothy Jason <timothycaviezel@gmail.com>
2024-04-28 11:28:39 +0700

    fourth commit

--- src/lib/database/models/user.model.ts
+++ src/lib/database/models/user.model.ts
@@ -1,12 +1,12 @@
 import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
-    clerkId: {type: String, required: true, unique: true},
+    clerkId: {type:String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
-    username: {type: String, required: true, unique: true},
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    photo: {type: String, required: true}
+    username: {type: String, unique: true},
+    photo: {type: String, required: true},
+    firstName: {type: String},
+    lastName: {type: String}
 })
 
 const User = models.User || model("User", UserSchema)
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,11 @@
-import {
-  clerkMiddleware,
-  createRouteMatcher
-} from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
   '/forum',
 ]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
